a37bba08e2272731c5601a41011b6dc14d9c2148
kretov.em <kretov.em@yandex.ru>
2019-07-29 21:42:32 +0300

    Знакомимся со списками

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,6 +16,20 @@ namespace snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int item in numList)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
 
